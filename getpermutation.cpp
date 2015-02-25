5f6d22f1142603a8e571f51248686de96f052a48
Xiaodong Zha <xiaodong.zha@outlook.com>
2015-02-25 21:56:44 +0800

    new impl

--- getpermutation.cpp
+++ getpermutation.cpp
@@ -1,55 +1,32 @@
-#include <iostream>
 #include <vector>
 #include <string>
+#include <iostream>
+
 using namespace std;
 
-vector<string> GetPermutationRecurs(string sInput, int low, int high)
-{
-    vector<string> result;
-    if(low == high)    
-    {
-        result.push_back(sInput);
-        return result;    
-    }
+vector<string> GetPermutation(const string& s) {
+  if (s.empty() || s.length() == 1) return {s};
+  
+  auto result = vector<string>{};
+  for (auto i = 0; i < s.length(); ++i) {
+    auto permutation = s;
+    auto tmp = permutation[0];
+    permutation[0] = permutation[i];
+    permutation[i] = tmp;
 
-    vector<string> preResult = GetPermutationRecurs(sInput, low+1, high);
-    for(size_t i = 0; i < preResult.size(); ++i)
-    {
-        string base(preResult[i]); 
-        for(int j = low; j <= high; ++j)
-        {
-            string permutation(base);
-            char tmp = permutation[low];
-            permutation[low] = permutation[j];
-            permutation[j] = tmp;
-            result.push_back(permutation);
-        }
+    auto sub_permutation = GetPermutation(permutation.substr(1, permutation.length() - 1));
+    for (auto& sub : sub_permutation) {
+      result.push_back(string(permutation[0] + sub));
     }
-    
-    return result; 
+  }
+  
+  return result;
 }
 
-
-
-vector<string> GetPermutaion(string sInput)
-{
-    return GetPermutationRecurs(sInput, 0, sInput.length() - 1);    
-    
+int main(void) {
+  auto p = GetPermutation("abcd");
+  for (auto& s : p) {
+    cout << s << endl;
+  }
+  return 0;
 }
-
-int main(void)
-{
-    string s("abcd");
-    vector<string> output = GetPermutaion(s);
-    for(size_t i = 0; i < output.size(); ++i)
-    {
-        cout << output[i] << endl;    
-    }
-
-    return 0;
-    
-    
-    
-}
-
-
